a9bc726add8168f56ed314094b12816bafb443e5
7087647458 <sp1243095@gmail.com>
2024-05-09 16:51:32 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
+global.ownernumber = 7347637958,
+global.ownername = saksham and Piyush ,
+global.ytname = "YT:op
+global.socialm = "GitHub: 
 global.location = "Indian"
 
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
